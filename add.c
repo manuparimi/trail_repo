832661751345d0652630539e9b98b5da3a320c86
Manasa <parimimanasa2002@gmail.com>
2025-06-01 15:18:44 +0000

    changes in add.c

--- add.c
+++ add.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
+
+int add(int a, int b) {
+	return a+b;
+}
+
 int main()
 {
 	int a = 10;	
 	int b = 20;
-	printf("%d + %d is %d\n", a, b, a+b);
+	int res = add(a, b);
+	printf("%d + %d is %d\n", a, b, res);
 }
